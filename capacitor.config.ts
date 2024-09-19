a4accb1b9207790747314f96c037571e44285856
joemafu <joe_mafu@hotmail.com>
2024-09-18 21:17:49 -0300

    Trabajando en Splash Dinámico

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,25 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'relevamiento.visual',
   appName: 'Relevamiento Visual',
-  webDir: 'www'
+  webDir: 'www',
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 500,
+      launchAutoHide: true,
+      launchFadeOutDuration: 1500,
+      backgroundColor: "#000000",
+      androidSplashResourceName: "../../assets/icon/favicon.png",
+      androidScaleType: "CENTER_CROP",
+      showSpinner: true,
+      androidSpinnerStyle: "large",
+      iosSpinnerStyle: "small",
+      spinnerColor: "#999999",
+      splashFullScreen: true,
+      splashImmersive: true,
+      layoutName: "launch_screen",
+      useDialog: true,
+    },
+  },
 };
 
 export default config;
