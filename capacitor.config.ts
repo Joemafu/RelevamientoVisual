4a621ed11f826a2ac75f5a9a963b86c5cae14a25
joemafu <joe_mafu@hotmail.com>
2024-09-20 20:57:10 -0300

    Splash

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -6,20 +6,16 @@ const config: CapacitorConfig = {
   webDir: 'www',
   plugins: {
     SplashScreen: {
-      launchShowDuration: 500,
-      launchAutoHide: true,
-      launchFadeOutDuration: 1500,
-      backgroundColor: "#000000",
-      androidSplashResourceName: "../../assets/icon/favicon.png",
+      launchShowDuration: 0, // No mostrar el splash screen nativo
+      launchAutoHide: false, // No ocultar automáticamente
+      backgroundColor: "#ffffffff", // Color de fondo del splash screen
       androidScaleType: "CENTER_CROP",
-      showSpinner: true,
-      androidSpinnerStyle: "large",
-      iosSpinnerStyle: "small",
-      spinnerColor: "#999999",
+      showSpinner: true, // Mostrar un spinner de carga
+      //androidSpinnerStyle: "large", // Estilo del spinner en Android
+      //iosSpinnerStyle: "small", // Estilo del spinner en iOS
+      //spinnerColor: "#999999",
       splashFullScreen: true,
       splashImmersive: true,
-      layoutName: "launch_screen",
-      useDialog: true,
     },
   },
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,40 @@
 import { Routes } from '@angular/router';
+import { TabsPage } from './tabs/tabs.page';
 
 export const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./tabs/tabs.routes').then((m) => m.routes),
+    redirectTo: 'login',
+    pathMatch: 'full',
   },
+  {
+    path: 'login',
+    loadComponent: () => import('./components/login/login.page').then( m => m.LoginPage)
+  },
+  {
+    path: 'tabs',
+    component: TabsPage,
+    children: [
+      {
+        path: 'tab1',
+        loadComponent: () =>
+          import('./tab1/tab1.page').then((m) => m.Tab1Page),
+      },
+      {
+        path: 'tab2',
+        loadComponent: () =>
+          import('./tab2/tab2.page').then((m) => m.Tab2Page),
+      },
+      {
+        path: 'tab3',
+        loadComponent: () =>
+          import('./tab3/tab3.page').then((m) => m.Tab3Page),
+      },
+      {
+        path: '',
+        redirectTo: '/tabs/tab1',
+        pathMatch: 'full',
+      },
+    ],
+  }
 ];
